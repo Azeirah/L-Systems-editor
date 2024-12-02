4c36bb7b045f35519d56a2ec65fe7fc1b722712f
Laura Brekelmans <mail@laura.fm>
2024-12-02 14:07:26 +0100

    Add playwright to the nix flake

--- flake.nix
+++ flake.nix
@@ -17,10 +17,23 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             bun
+            playwright-driver.browsers
+            playwright-test
           ];
 
           shellHook = ''
-	    echo "run 'bun run dev' to get started :)"
+          echo "Welcome to the l-systems development environment!
+
+                For contact and questions
+                - contact laura at mail@laura.fm
+                - Or visit the github repository https://github.com/Azeirah/L-Systems-editor
+
+                Quick start tips
+                - Run 'bun run dev' to get started with development
+                - Run 'playwright' to open the test environment
+
+                Have fun with the trees :)
+          ";
           '';
         };
       });
