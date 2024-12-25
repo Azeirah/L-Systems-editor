c1d10f19e19da7fb9fd36fdc85e55b5f5f401e33
Laura Brekelmans <mail@laura.fm>
2024-12-25 21:18:44 +0100

    Updated flake with auto install deps

--- flake.nix
+++ flake.nix
@@ -8,7 +8,14 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        setup-script = pkgs.writeScriptBin "setup-lsystems" ''
+                  #!${pkgs.bash}/bin/bash
+          	if [[ ! -d node_modules ]]; then
+                    ${pkgs.bun}/bin/bun i
+          	fi
+        '';
       in {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
@@ -17,6 +24,7 @@
             # this package is necessary to run playwright with bun. I really don't know why.
             # See my comment on the issue: https://github.com/oven-sh/bun/issues/8222#issuecomment-2512722612
             nodejs_22
+            setup-script
           ];
 
           buildInputs = with pkgs; [ bun ];
@@ -24,20 +32,21 @@
           PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
 
           shellHook = ''
+            setup-lsystems
             ${pkgs.gum}/bin/gum format <<EOF
             # Welcome to the L-systems development environment!
 
-            Don't forget to run \`bun install\`
-
             For contact and questions
-            - contact laura at mail@laura.fm
-            - Or visit the github repository https://github.com/Azeirah/L-Systems-editor
+
+            - Contact laura at mail@laura.fm
+            - Or [visit the github repository](https://github.com/Azeirah/L-Systems-editor)
 
             ## Quick start tips
 
             - Run \`bun run dev\` to get started with development
 
             ### Testing
+
             - Run 'bun run test' to run all tests
             - Run 'bun run test:ui' to open the test UI
 
